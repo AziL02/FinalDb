ea348d52cc499212610cc779f903161d8cd4893e
AziL02 <jllegends0619@gmail.com>
2022-01-05 09:50:19 +0800

    Final

---
+++ src/main/java/com/example/FINALEEAWS/Customers/CustomersRepository.java
@@ -0,0 +1,9 @@
+package com.example.FINALEEAWS.Customers;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomersRepository
+        extends JpaRepository<Customers, Long> {
+}
---
+++ src/test/java/com/example/FINALEEAWS/FinaleeawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FINALEEAWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinaleeawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
